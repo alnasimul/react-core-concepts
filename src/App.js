331e6a9399e183230d363784a8c85f60cfc1c345
alnasimul <alnasimul@gmail.com>
2020-04-24 00:13:53 +0600

    first compenent create using react js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My first react paragraph</p>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
 
+function Person(){
+  const personStyle = {
+    color:'cyan',
+    border: '1px solid gray',
+    margin: '20px 20px',
+    padding: '20px 20px',
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: Al Nasimul Haque </h1>
+      <h3>Profession: Web Developer</h3>
+    </div>
+  )
+}
 export default App;
